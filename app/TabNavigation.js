c5e0901f4dfa51cfa62ee18402efb388e15737d0
MoathDawahreh <moath.dawahreh@gmail.com>
2021-02-08 21:48:17 +0200

    TabNav

--- app/TabNavigation.js
+++ app/TabNavigation.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {Image,StyleSheet} from 'react-native'
+import {Image,StyleSheet,Dimensions} from 'react-native'
 import ProfileScreen from './ProfileScreen'
 import ShoppingItemsList from '../app/screens/ShoppingItemsList'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useIsFocused } from '@react-navigation/native';
+import { useIsFocused} from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,17 +15,29 @@ export default class TabNav extends React.Component{
 
 render(){
 
+
+  // console.log(Dimensions.get('window'))
+
     return(
 
+      
         <Tab.Navigator >
-        <Tab.Screen name="Shope" component={ShoppingItemsList} />
+        <Tab.Screen name="Shope" component={ShoppingItemsList}
+         options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: () => (
+            <Image style={styles.img} source={require('./assets/shopping.png')} />
+          ),
+          tabBarBadge: 2,
+          }}
+        
+        />
         <Tab.Screen name="Settings" component={ProfileScreen} 
           options={{
             tabBarLabel: 'Profile',
             tabBarIcon: ({ color, size }) => (
-              <Image style={styles.img} source={require('./assets/shopping.png')} />
+              <Image style={styles.img} source={require('./assets/profile.png')} />
             ),
-            tabBarBadge: 2,
             }} />
       </Tab.Navigator>
 
