799eaa8bbb8263277f9f28d391b69cec0c36edb6
MoathDawahreh <moath.dawahreh@gmail.com>
2021-02-22 14:56:24 +0200

    shift to expo

--- App.js
+++ App.js
@@ -1,96 +1,21 @@
-import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import HomeScreen from './app/home'
-
-import LoginForm from './app/screens/LoginScreen'
-import ShoppingItemsList from './app/screens/ShoppingItemsList'
-import ItemDetails from './app/ItemDetails'
-import TabNav from './app/TabNavigation'
-
-
-const Stack = createStackNavigator();
-
-
-const App = () => {
-
-
+export default function App() {
   return (
-
-      <NavigationContainer>
-      
-      <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="LogIn" component={LoginForm} options = {{title: 'Back'}} />
-        {/* <Stack.Screen name = 'ShoppingItemsList' component={ShoppingItemsList}/>  */}
-        <Stack.Screen name="TabNav" component={TabNav}    options={{ title: 'home',    headerLeft: null }}
- />
-
-        <Stack.Screen name='ItemDetails' component={ItemDetails}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
